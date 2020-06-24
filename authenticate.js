3636c9980930e2b56bc7f2b2a5974ef733b9cfc8
Piyush Bankar <piyushbankar18@gmail.com>
2020-06-24 23:05:05 +0530

    passport authentication

---
+++ authenticate.js
@@ -0,0 +1,5 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
